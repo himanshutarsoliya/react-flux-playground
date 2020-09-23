cc9b2fea073cc361fad99e4387c572f721e94139
Tarsoliya,H,Himanshu,TAQ5 C <himanshu.tarsoliya@bt.com>
2020-09-23 13:51:23 +0530

    Flux, Action, Dispatcher, Store

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { getCourses } from "../api/courseApi";
+import courseStore from "../stores/courseStore";
 import CourseList from "./CourseList";
 import { Link } from "react-router-dom";
+import { loadCourses, deleteCourse } from "../actions/courseActions";
 
 function CoursesPage() {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState(courseStore.getCourses());
 
   useEffect(() => {
-    getCourses().then((courses) => setCourses(courses));
+    courseStore.addChangeListner(onChange);
+    if (courseStore.getCourses().length === 0) loadCourses();
+    return () => courseStore.removeChangeListner(onChange);
   }, []);
+
+  function onChange() {
+    setCourses(courseStore.getCourses());
+  }
 
   return (
     <>
@@ -16,7 +23,7 @@ function CoursesPage() {
       <Link className="btn btn-primary" to="/course">
         Add Course
       </Link>
-      <CourseList courses={courses} />
+      <CourseList courses={courses} deleteCourse={deleteCourse} />
     </>
   );
 }
